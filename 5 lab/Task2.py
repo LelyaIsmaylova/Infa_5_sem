dacb30ace38dd47770bcbcb70a9c45a48060c665
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 23:27:11 +0500

    Update Task2.py

--- 5 lab/Task2.py
+++ 5 lab/Task2.py
@@ -1,1 +1,18 @@
+class Shape:
+    def init(self, w, h) -> None:
+        self.w = w
+        self.h = h
 
+class Triangle(Shape):
+    def get_area(self):
+        return 0.5 * self.w * self.h
+
+class Rectangle(Shape):
+    def get_area(self):
+        return self.w * self.h
+    
+rec = Rectangle(20, 40)
+tri = Triangle(20, 40)
+
+print(rec.get_area())
+print(tri.get_area())
