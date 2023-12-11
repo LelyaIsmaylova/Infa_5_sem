502cdc2eca53dcbcfe39792c6f7b55a90dad0b2f
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 22:35:04 +0500

    Update Task3.py

--- 3 lab/Task3.py
+++ 3 lab/Task3.py
@@ -1,1 +1,16 @@
+import os
 
+def find_directories(root_dir):
+    py_dir = set()
+    for root, _, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith('.py'):
+                py_dir.add(root)
+    return sorted(py_dir)
+
+root_dir = '.'
+py_dir = find_directories(root_dir)
+
+with open('Task3.txt', 'w') as result_file:
+    for dir in py_dir:
+        result_file.write(dir + '\n')
