8b01ee8d563e840f721c7e8f1fa68454b7243c4d
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 22:38:26 +0500

    Update Task4.py

--- 3 lab/Task4.py
+++ 3 lab/Task4.py
@@ -1,1 +1,7 @@
+x = "wrong number"
+try:
+    number = int(x)
+except ValueError as e:
+    print("Произошла ошибка ValueError:", str(e))
 
+Произошла ошибка ValueError: invalid literal for int() with base 10: 'wrong number'
