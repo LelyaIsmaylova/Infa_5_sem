7f9d7811612ee90abb8201009ed8c7eb11e05b40
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 23:29:59 +0500

    Update Task4.py

--- 5 lab/Task4.py
+++ 5 lab/Task4.py
@@ -1,1 +1,24 @@
+class Animal:
+    def init(self, name, age):
+        self._name = name
+        self._age = age
 
+    def description(self):
+        return f"Name: {self._name}, Age: {self._age}"
+
+
+class Zebra(Animal):
+    def description(self):
+        return f"Name: {self._name}, Age: {self._age}, Species: Zebra"
+
+
+class Dolphin(Animal):
+    def description(self):
+        return f"Name: {self._name}, Age: {self._age}, Species: Dolphin"
+
+
+zebra = Zebra("Stripes", 5)
+dolphin = Dolphin("Dolly", 7)
+
+print(zebra.description())
+print(dolphin.description())
