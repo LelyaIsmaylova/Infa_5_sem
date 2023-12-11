272a812c3bd7e29c2df5a5e0cf55819ace0d3fef
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 23:28:05 +0500

    Update Task3.py

--- 5 lab/Task3.py
+++ 5 lab/Task3.py
@@ -1,1 +1,13 @@
+class Mother:
+    def str(self):
+        return "Это материнский класс"
 
+class Daughter(Mother):
+    def str(self):
+        return "Это дочерний класс"
+
+mother = Mother()
+daughter = Daughter()
+
+print(mother)
+print(daughter)
