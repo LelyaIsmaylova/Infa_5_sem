97740e097736127f90d92efc61dc6b297cd6d13f
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-12 00:00:40 +0500

    Update 9.py

--- 8 lab/9.py
+++ 8 lab/9.py
@@ -1,1 +1,14 @@
+def maximize(lists, m):
+    max_result = 0
+    for combo in product(*lists):
+        result = sum(x**2 for x in combo) % m
+        max_result = max(max_result, result)
+    print(max_result)
+  
+lists = [
+    [5, 4],
+    [7, 8, 9],
+    [5, 7, 8, 9, 10]
+]
 
+maximize(lists, m=1000)
