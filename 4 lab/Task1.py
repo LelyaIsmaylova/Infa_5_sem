a08468b1d0dc335e22ab810dc5f5fec38e39f0ec
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 22:55:58 +0500

    Update Task1.py

--- 4 lab/Task1.py
+++ 4 lab/Task1.py
@@ -1,1 +1,12 @@
+import sys
 
+def fibonacci(n):
+    fibonacci_sequence = [0, 1]
+    if n <= 0:
+        return []
+    while len(fibonacci_sequence) < n:
+        next_member = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fib_sequence.append(next_member)
+    return fibonacci_sequence[-1]
+
+print(fibonacci(int(sys.argv[-1])))
