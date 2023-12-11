859fa3c4a4c566461dbe243be97464f450bbc662
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-12 00:31:58 +0500

    Update Task4.py

--- 9 lab/Task4.py
+++ 9 lab/Task4.py
@@ -1,1 +1,33 @@
+def print_coroutine():
+    a = "start"
+    b = 0
+    l = []
+    c = 0
+    d = 0
+    count = 0
+  
+    while True:
+        a = yield a
+        if a.isdigit():
+            a = int(a)
+            b += a
+            l.append(a)
+            count = len(l)
+            c = b / count
+            d = b / count - c 
+            print("Got value", a)
+        else:
+            if a == 'mean':
+                print('mean', c)
+            elif a == 'var':
+                print('var', d)
+            elif a == 'count':
+                print('count', count)
 
+
+coroutine = print_coroutine()
+print(neat(coroutine))
+
+while True:
+    n = input()
+    print(coroutine.send(n))
