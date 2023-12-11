47216c0ee266d168b11f5b9e360da7fbb59f1b6b
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 23:59:39 +0500

    Update 8.py

--- 8 lab/8.py
+++ 8 lab/8.py
@@ -1,1 +1,7 @@
+from itertools import groupby
 
+def compress_string(s):
+    compressed = [(len(list(group)), int(key)) for key, group in groupby(s)]
+    print(compressed)
+
+compress_string('12345')
