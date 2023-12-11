4b001a8c6b72b8fd6874dd082aed0af7a06f9c3b
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 23:57:07 +0500

    Update 6.py

--- 8 lab/6.py
+++ 8 lab/6.py
@@ -1,1 +1,10 @@
+from itertools import combinations
 
+def get_combinations(s, n):
+    result = []
+    for k in range(1, n + 1):
+        for combo in combinations(s, k):
+            result.append(''.join(combo))
+    print(result)
+
+get_combinations("doctor", 2)
