f20ba031d020df7aec28a895a42f037bc13e8604
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 23:56:22 +0500

    Update 5.py

--- 8 lab/5.py
+++ 8 lab/5.py
@@ -1,1 +1,7 @@
+from itertools import permutations
+def get_permutations(s, n):
+    result = [''.join(p) for p in permutations(s, n)]
+    result.sort()
+    print(result)
 
+get_permutations("doctor", 2)
