c7066ba8d94722ca01b13ec40bdcb45d20476eb7
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 22:27:32 +0500

    Update Task2.py

--- 3 lab/Task2.py
+++ 3 lab/Task2.py
@@ -2,4 +2,4 @@ def write_array(array, file_name):
     with open(file_name, 'w') as file:
         file.writelines(array)
 
-write_array(['TARDIS\n', 'Doctor\n', 'Donna\n'], 'DW.txt')
+write_array(['TARDIS\n', 'Doctor\n', 'Donna\n'], 'Task2.txt')
