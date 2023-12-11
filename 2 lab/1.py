65f7e98fc038fcba20000678e5e6ec22367d605a
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 19:58:30 +0500

    Update 1.py

--- 2 lab/1.py
+++ 2 lab/1.py
@@ -1,19 +1,6 @@
-maximum = None
-
-while True:
-    num = input("Введите элементы последовательности: ")
-    if num == "End":
-        break
-
-    try:
-        N = float(num)
-        if maximum is None or N > maximum:
-            maximum = N
-
-    except ValueError:
-        print("Последовательность была задана некорректно.")
-
-if maximum is not None:
-    print("Максимальное число последовательности:", maximum)
-else:
-    print("Последовательность была задана некорректно.")
+#! /bin/bash
+cd ~
+mkdir -p Desktop/hello_world
+cd Desktop/hello_world
+echo 'Hello world!' > absolute.txt
+ls -l
