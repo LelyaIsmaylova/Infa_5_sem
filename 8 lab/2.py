9c5653ada2f39d04979f8cade728471d9fa4e7d8
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 23:46:38 +0500

    Update 2.py

--- 8 lab/2.py
+++ 8 lab/2.py
@@ -1,1 +1,11 @@
+def fibonacci_sequence(n):
+    a, b = 0, 1
+    count = 0
+  
+    while count < n:
+        yield a
+        a, b = b, a + b
+        count += 1
 
+for i in fibonacci_sequence(10):
+    print(i)
