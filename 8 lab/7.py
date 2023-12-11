9b257d4e673faeba49f3f1ff1d28994ed360608d
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-11 23:57:58 +0500

    Update 7.py

--- 8 lab/7.py
+++ 8 lab/7.py
@@ -1,1 +1,10 @@
+from itertools import combinations, combinations_with_replacement
 
+def get_combinations(s, n):
+    result = []
+    for k in range(1, n + 1):
+        for combo in combinations(s, k):
+            result.append(''.join(combo))
+    print(result)
+
+get_combinations("doctor", 2)
