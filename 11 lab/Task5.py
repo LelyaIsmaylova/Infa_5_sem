db884fe1db37be5a80771e69fc03ccee0c6a92ef
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-12 00:57:31 +0500

    Update Task5.py

--- 11 lab/Task5.py
+++ 11 lab/Task5.py
@@ -1,1 +1,22 @@
+Запустите код. Попробуйте объяснить, почему LIST - пуст.
 
+import multiprocessing
+
+
+def worker():
+    LIST.append('item')
+
+
+LIST = []
+
+
+if __name__ == "__main__":
+    processes = [
+        multiprocessing.Process(target=worker)
+        for _ in range(5)
+    ]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+    print(LIST)
