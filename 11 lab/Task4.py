221ff10ede9246165ac98b3a21cd87b29fde2de8
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-12 00:56:51 +0500

    Update Task4.py

--- 11 lab/Task4.py
+++ 11 lab/Task4.py
@@ -1,1 +1,33 @@
+#2.68 seconds
+import urllib.request
+import time
 
+urls = [
+    'https://www.yandex.ru', 'https://www.google.com',
+    'https://habrahabr.ru', 'https://www.python.org',
+    'https://isocpp.org',
+]
+
+def read_url(url):
+    with urllib.request.urlopen(url) as u:
+        return u.read()
+
+start = time.time()
+for url in urls:
+    read_url(url)
+  
+print(time.time() - start)
+
+result = {}
+threads = []
+start = time.time()
+
+for url in urls:
+    thread = threading.Thread(target=read_url, args=(url,))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+print(f"Total time: {time.time() - start} seconds")
